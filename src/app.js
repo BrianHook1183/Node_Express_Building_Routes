165778acf175475adf42a46915c46d4ee57f2f2f
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-05-08 16:02:00 -0500

    submitted solution

--- src/app.js
+++ src/app.js
@@ -2,6 +2,15 @@ const { NODE_ENV = "development" } = process.env;
 const express = require("express");
 const app = express();
 
-// Build your routes here
+const sayPong = (req, res) => {
+  res.send("pong!");
+};
+
+const sayWelcome = (req, res) => {
+  res.send("Welcome to my server.");
+};
+
+app.get("/ping", sayPong);
+app.get("/welcome", sayWelcome);
 
 module.exports = app;
